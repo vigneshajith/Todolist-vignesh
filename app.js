aaa8b773186add71edee90da8ed7bf5966f442ed
ajith vignesh <vigneshajith152@gmail.com>
2023-02-02 13:15:49 +0530

    port was update

--- app.js
+++ app.js
@@ -123,6 +123,8 @@ app.get("/:listname", function (req, res) {
 
 app.get("/about", function (req, res) {});
 
-app.listen(3000, function () {
+const port = process.evn.PORT || 3000
+
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
