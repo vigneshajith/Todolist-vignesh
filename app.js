4ad2ae95d850c70ae359cb934770b840cb48b008
ajith vignesh <vigneshajith152@gmail.com>
2023-02-02 13:18:21 +0530

    port is add in 129

--- app.js
+++ app.js
@@ -126,5 +126,5 @@ app.get("/about", function (req, res) {});
 const port = process.evn.PORT || 3000;
 
 app.listen(port, function () {
-  console.log("Server started on port 3000");
+  console.log("Server started on port "+port);
 });
